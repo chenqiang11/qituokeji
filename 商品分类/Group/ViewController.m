3ea2d7e466c2b8513bc18085b298eb9384c26641
陈强 <879752542@qq.com>
2018-09-26 13:05:23 +0800

    no message

--- 商品分类/Group/ViewController.m
+++ 商品分类/Group/ViewController.m
@@ -29,7 +29,7 @@
     [self CreatRightCollectionView];
     UIButton * btn =  [UIButton buttonWithType:UIButtonTypeCustom ];
     btn.frame = CGRectMake(0, 0, 0, 0   );
-    btn.frame =  CGRectMake(30, 39, 30, 30);
+    btn.frame =  CGRectMake(30, 39444444, 30, 30);
 }
 -(void)LeftTableView{
     
